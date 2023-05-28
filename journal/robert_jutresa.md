56585bf84567fff1e4c9cac960ce58dae488f3d5
RobertJ <r.jutresa@gmail.com>
2023-05-28 23:41:41 +0200

    Final MVP fixes, journal update - rj

--- journal/robert_jutresa.md
+++ journal/robert_jutresa.md
@@ -1,6 +1,6 @@
 # Robert Jutreša's Data Science Project Competition journal
 
-## March 2022 (8h)
+## March 2022 (9h 30min)
 
 * **1st (1h)**: Meeting with everyone - quick conversation about the data we are working with, establishing expectations, deciding on preliminary goals, setting up feature meetings and weekly schedule.
 * **6th (2h)**: Working with my coworker - we looked at the agreed upon data, outlined important and numerical data, found some extraction tools for images, tables, text etc. We tested some, but not all, of them.
@@ -10,7 +10,7 @@
 * **20th (1h)**: Preparing the cleaned version of the extracted questions and answers for the client. Send it over e-mail (1h).
 * **30th (30 min)**: Meeting with NLB - talked about the clean data, got an updated set. Talked about Cloud Computing platform with the advisor from the industry and got access to it. Made a plan for the next week.
 
-## April 2022 (49h)
+## April 2022 (39h)
 * **4th (6h)**: Working with my coworker - getting a baseline comparison of predictions from a model without fine tunning, working on the environment, and preparing a feature plan for development and testing of the main model.
 * **5th (8h)**: Working with my coworker, meeting with the professor - original fine tuning, setting up a Sequence Extractive model, planning the next steps.
 * **11th (8h)**: Working with my coworker - setting up a extractive model, starting the fine tunning process, further research, developing a plan.
@@ -19,7 +19,7 @@
 * **14th (2h)**: Code/comment management, working environment assurance, report rereading, merging the development branch into master.
 * **15th (1h)**: Interim report, minor changes, content updates based on advisor feedback and references fixes.  
 
-## May 2022 (73h)
+## May 2022 (85h)
 * **5th (6h)**: Working by myself - automatic question - answer - context generation for the 2022 report, collaboration with the client for a better dataset and setting plans for executing the plan.
 * **9th (8h)**: Working with my coworker - setting up the full stable conda environment, testing functionalities of all scripts, setting up functional FAISS document store, building the full query - context extraction - answer generation pipeline.
 * **10th (6h)**: Meeting with the client - talking about the final data (we should get it by the end of the week), planning the final report, setting up the full generative pipeline, making an MVP for model usage.
@@ -31,5 +31,6 @@
 * **23rd (6h)**: Working by myself - pipeline diagram, report fixing and finalization.
 * **24rd (3h)**: Working by myself - fixing report from clients feedback.
 * **27th (11h)**: Working with my coworker and by myself - working on the report, presentation and SE analysis.
+* **28th (12 30min)**: Working by myself and with my coworker - report proofreading, presentation finalization, README.md setup, repository and code cleanup, final code touches, environment testing etc.
 
-## Total: [total sum of hours]
+## Total: [134h]
--- src/app/model.py
+++ src/app/model.py
@@ -6,10 +6,11 @@ from haystack.pipelines import Pipeline
 import pathlib as pl
 
 
-def build_document_store(year):
+def build_document_store():
     converter = PDFToTextConverter(remove_numeric_tables=True)
     extracted = converter.convert(file_path=pl.Path(
-        f"../data/raw/sustainability-report-{year}.pdf"), meta=False, encoding="UTF-8")[0]
+        f"../data/raw/sustainability-report-2042.pdf"), meta=False, encoding="UTF-8")[0]
+    
     preprocessor = PreProcessor(
         clean_empty_lines=True,
         clean_whitespace=True,
@@ -20,15 +21,18 @@ def build_document_store(year):
         split_overlap=0
     )
     cleaned = preprocessor.process([extracted])
+
     document_store = FAISSDocumentStore(
         faiss_index_factory_str='Flat', similarity="dot_product")
     document_store.write_documents(cleaned)
+
     retriever = DensePassageRetriever(
         document_store=document_store,
-        query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-        passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
+        query_embedding_model="SpeedaRJ/dpr-fb-finetuned-nlb-question-encoder",
+        passage_embedding_model="SpeedaRJ/dpr-fb-finetuned-nlb-context-encoder",
         use_gpu=True,
     )
+
     document_store.update_embeddings(retriever)
     document_store.save("document_store.faiss")
 
@@ -37,27 +41,22 @@ def read_document_store():
     return FAISSDocumentStore.load(index_path="document_store.faiss", config_path="document_store.json")
 
 
-def build_retriever(ds, year, dpr):
-    if dpr:
-        retriever = DensePassageRetriever.load(load_dir=f"../../data/models/DPR/{year}", document_store=ds, use_gpu=True)
-    else: 
-        retriever = DensePassageRetriever(
-            document_store=ds,
-            query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-            passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
-            use_gpu=True
-        )
+def build_retriever(ds):
+    retriever = DensePassageRetriever(
+        document_store=ds,
+        query_embedding_model="SpeedaRJ/dpr-fb-finetuned-nlb-question-encoder",
+        passage_embedding_model="SpeedaRJ/dpr-fb-finetuned-nlb-context-encoder",
+        use_gpu=True
+    )
     return retriever
 
 
-def build_reader(year, db):
-    path = "../data/models/BERT/"
-    return TransformersReader(model_name_or_path=f"{path}distilbert-base-cased-distilled-squad-finetuned-NLB-QA-{year}-{db}", use_gpu=True)
+def build_reader():
+    return TransformersReader("SpeedaRJ/roberta-base-nlb-finetuned", use_gpu=True)
 
 
-def build_generator(year, db):
-    path = "../data/models/T5/"
-    return Seq2SeqGenerator(model_name_or_path=f"{path}t5-small-finetuned-squadv2-finetuned-NLB-QA-{year}-{db}", input_converter=_BartEli5Converter())
+def build_generator():
+    return Seq2SeqGenerator("SpeedaRJ/t5-base-nlb-finetuned", input_converter=_BartEli5Converter(), use_gpu=True)
 
 
 def build_pipeline(model, retriever):
